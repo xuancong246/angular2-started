a6796d879a0349d80045f4045df900d4f262a296
xuancong246 <xuancong246@yahoo.com>
2016-05-31 22:44:04 +0700

    Simple component with Angular2, bootstrap Angular application

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pm-app',
+    template: "Hello world! Welcome to Angular2 getting started."
+})
+export class AppComponent { //Setup tsconfig.json to ...
+    //Define properties and methods    
+}
